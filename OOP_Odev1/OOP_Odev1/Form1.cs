b97941e1fc0eb535f3843f68797d42c1b9df419e
bgryks18@gmail.com <bgryks18@gmail.com>
2020-12-22 01:46:04 +0300

    ödev dosyaları

---
+++ OOP_Odev1/OOP_Odev1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Odev1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            masa masa1 = new masa();
+
+            masa1.genislik = 24.4;
+            masa1.yukseklik = 23.4;
+            string format = string.Format("Yükekliği: {0}, Genişliği: {1}",masa1.yukseklik,masa1.genislik);
+            MessageBox.Show(format);
+        }
+    }
+}
---
+++ OOP_Odev1/OOP_Odev1/class.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Odev1
+{
+    class masa
+    {
+        public double yukseklik;
+        public double genislik;
+        public string renk;
+    }
+    class dumbell{
+        public double kg;
+        public string renk;
+        public bool agirlik_arttirma;
+    }
+
+    class ayakkabi
+    {
+        public double numara;
+        public string renk;
+        public decimal fiyat;
+
+    }
+
+    class kulaklik
+    {
+        public decimal fiyat;
+        public string renk;
+        public bool kablolu_mu;
+    }
+    class hali{
+        public double en;
+        public double boy;
+        public string renk;
+    }
+    class kolonya
+    {
+        public decimal fiyat;
+        public string aroma;
+        public double litre;
+    }
+    class pantolon
+    {
+        public decimal fiyat;
+        public string renk;
+        public string beden;
+
+    }
+    class canta
+    {
+        public decimal fiyat;
+        public string renk;
+        public string marka;
+    }
+    class atlamaipi
+    {
+        public decimal fiyat;
+        public string renk;
+        public double boy;
+    }
+    class mousepad
+    {
+        public double en;
+        public double boy;
+    }
+    class usbkablo
+    {
+        public double boy;
+        public string renk;
+        public bool type_c;
+    }
+
+    class flashbellek
+    {
+        public decimal fiyat;
+        public string gbyte;
+
+    }
+    class coklupriz
+    {
+        public int kacbolmeli;
+    }
+    class dolap
+    {
+        public string renk;
+        public double en;
+        public double boy;
+    }
+    class oyuncakayi
+    {
+        public string renk;
+    }
+}
